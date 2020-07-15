5b387e81eede8fb84354e2f5777c626c2e76471d
dci <wafi.amer.17@gmail.com>
2020-07-16 01:21:14 +0200

    Updates

---
+++ precache-manifest.eb837c12372777f08e8dc109bf3b0492.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd20ba785510b6009a429b071b5a1ff",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "73487bbeaf28aff6d3fa",
+    "url": "/MovieApp/static/js/2.b9b394e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieApp/static/js/2.b9b394e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8aa80ef60ca7cbaff23",
+    "url": "/MovieApp/static/js/main.4f7e3f2c.chunk.js"
+  },
+  {
+    "revision": "85806c50601933509fa7",
+    "url": "/MovieApp/static/js/runtime-main.311cbed2.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/MovieApp/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/MovieApp/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);

0eef0d46b0797d269d8e0acb453dcd79c96412d6
dci <wafi.amer.17@gmail.com>
2020-07-24 00:27:22 +0200

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <>
       <Header />
       <Router>
-        <Home path="/" />
+        <Home path="MovieApp/" />
         <Movie path="/:movieId" />
         <NotFound default />
       </Router>

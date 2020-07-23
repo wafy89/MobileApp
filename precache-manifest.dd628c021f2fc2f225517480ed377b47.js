79e144426d010451ad87ef62132f539bf97abfc0
dci <wafi.amer.17@gmail.com>
2020-07-23 23:54:10 +0200

    Updates

--- precache-manifest.dd628c021f2fc2f225517480ed377b47.js
+++ precache-manifest.dd628c021f2fc2f225517480ed377b47.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92f5441323f21209fd77b885ce686c72",
+    "revision": "0b150ac7c8d50a866383e272a36332f2",
     "url": "/MovieApp/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MovieApp/static/js/2.b9b394e4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "60499d65c45c6484d6ed",
-    "url": "/MovieApp/static/js/main.832512d1.chunk.js"
+    "revision": "2a3100f93fa874d2435c",
+    "url": "/MovieApp/static/js/main.5ff294c4.chunk.js"
   },
   {
     "revision": "85806c50601933509fa7",
